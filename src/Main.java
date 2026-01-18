c3361aaad9a34342c1f96ad82d80390646b51ded
GiuGab <gabrielpf080806@gmail.com>
2026-01-18 18:20:47 -0300

    Adicionando o código principal (Main)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,61 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner leitura = new Scanner(System.in);
+        List<Compras> listaCompras = new ArrayList<>();
+        boolean loop = false;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Digite o limite do cartao: ");
+        int limiteCartao = leitura.nextInt();
+        leitura.nextLine();
+        Cartao cartao = new Cartao(limiteCartao);
+        int faturaCartao = 0;
+
+        while (!loop){
+            System.out.println("Digite a descricao da compra: ");
+            String descricaoCompra = leitura.nextLine();
+
+            System.out.println("Digite o valor da compra: ");
+            int valorCompra = leitura.nextInt();
+
+            if(faturaCartao + valorCompra > cartao.getLimiteCartao()){
+                System.out.println("Saldo insuficiente!");
+                System.out.println();
+                System.out.println("Compras realizadas:");
+
+                Collections.sort(listaCompras);
+                for (Compras compras : listaCompras){
+                    System.out.println(compras);
+                }
+                System.out.println();
+                System.out.println("Fatura do cartao: " + faturaCartao);
+                break;
+
+            }else{
+                faturaCartao += valorCompra;
+                Compras compras = new Compras(descricaoCompra, valorCompra);
+                listaCompras.add(compras);
+            }
+
+            System.out.println("Digite 0 para sair ou 1 para continuar: ");
+            int parar = leitura.nextInt();
+            leitura.nextLine();
+            if(parar == 0){
+                Collections.sort(listaCompras);
+                for(Compras compras : listaCompras){
+                    System.out.println(compras);
+                }
+                System.out.println();
+                System.out.println("Fatura do cartao: " + faturaCartao);
+                loop = true;
+            }else{
+                continue;
+            }
         }
+
     }
 }
